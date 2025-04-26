5e2341bdce198afada8f9fbf456a66debadb4974
Taoufiq ELHANCHAOUI <t.elhanchaoui@gmail.com>
2025-04-26 22:28:11 +0100

    ok

--- models/Appointment.model.js
+++ models/Appointment.model.js
@@ -2,37 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Appointment = new Schema(
-{
+  {
     dateTime: {
-        type: Date,
-        required: true,
-        trim: true,
+      type: Date,
+      required: true,
+      trim: true,
     },
     status: {
-        type: String,
-        required: true,
-        trim: true,     
+      type: String,
+      enum: ["Pending", "Confirmed", "Cancelled", "Completed", "Unconfirmed"],
+      default: "Pending",
     },
     loginMedcine: {
-        type: String,
-        required: true,
-        trim: true,     
+      type: String,
+      required: true,
+      trim: true,
     },
-    patient: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Patient',
-        required: true 
-      },
-      medicine: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Medicine',
-        required: true 
-      },
-},
- {
-    versionKey: false
-});
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    medicine: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Medicine",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-
-const AppointmentList = mongoose.model("Appointment",Appointment);
-module.exports=AppointmentList;
+const AppointmentList = mongoose.model("Appointment", Appointment);
+module.exports = AppointmentList;
