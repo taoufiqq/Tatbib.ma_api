f8acafc66be7814973c90a1c7181ba0a7aec3cd6
Taoufiq ELHANCHAOUI <t.elhanchaoui@gmail.com>
2025-04-27 23:54:59 +0100

    add t to route

--- routes/Appointment.router.js
+++ routes/Appointment.router.js
@@ -4,7 +4,7 @@ const AppointmentController = require('../controllers/Appointment.controller');
 
 router.get('/getAllAppointment',AppointmentController.getAllAppointment);
 router.post('/addAppointment',AppointmentController.addAppointment);
-router.get('/getAppointmenPatient/:id', AppointmentController.getAppointmentPatient);
+router.get('/getAppointmentPatient/:id', AppointmentController.getAppointmentPatient);
 router.get('/getAppointmenById/:id', AppointmentController.getAppointmentById);
 router.get('/getAppointmentMedcine/:id', AppointmentController.getAppointmentMedcine);
 router.get('/getAppointmentSecretary/:loginMedcine', AppointmentController.getAppointmentSecretary);
